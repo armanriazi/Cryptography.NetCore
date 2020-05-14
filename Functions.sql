99de0ca3b115619b8601beb5246084762bc30052
armanriazi <armanriazi@hotmail.com>
2020-05-14 21:31:21 +0000

    Update Functions.sql

--- Functions.sql
+++ Functions.sql
@@ -1,15 +1,15 @@
 USE [SemOperationWebService]
 GO
 --, @encrypted nvarchar(max) out, @extractedIVTxt nvarchar(max) out, @error nvarchar(max) out
-CREATE FUNCTION [dbo].[NikFuncEncryptionMain](@plainTxt [nvarchar](max))
+CREATE FUNCTION [dbo].[FuncEncryptionMain](@plainTxt [nvarchar](max))
  RETURNS  nvarchar(max) 
  WITH EXECUTE AS CALLER
 AS 
-EXTERNAL NAME [NikCryptographyMain].[NikCryptographyMain.Encryption].[NikFuncEncryptionMain]
+EXTERNAL NAME [CryptographyMain].[CryptographyMain.Encryption].[FuncEncryptionMain]
 GO
 
-CREATE FUNCTION [dbo].[NikFuncDecryptionMain](@encrypted [nvarchar](max), @keyTxt [nvarchar](max))
+CREATE FUNCTION [dbo].[FuncDecryptionMain](@encrypted [nvarchar](max), @keyTxt [nvarchar](max))
 RETURNS [nvarchar](max) WITH EXECUTE AS CALLER
 AS 
-EXTERNAL NAME [NikCryptographyMain].[NikCryptographyMain.Decryption].[NikFuncDecryptionMain]
+EXTERNAL NAME [CryptographyMain].[CryptographyMain.Decryption].[FuncDecryptionMain]
 GO
